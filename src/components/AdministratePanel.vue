c57a9236732257954f1039c015e630b17fe9b74e
Termtime <49447170+Termtime@users.noreply.github.com>
2020-07-23 22:09:06 -0600

    Creacion de panel administrativo

---
+++ src/components/AdministratePanel.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="adminModal"
+      data-backdrop="static"
+      data-keyboard="false"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">
+              Administrar Libros
+            </h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Cerrar
+            </button>
+            <button type="button" class="btn btn-primary">Understood</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "AdministratePanel"
+};
+</script>
